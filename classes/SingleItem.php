2518cc07a183063512259986a62d962b4496da3c
James Gedny <james.gedny@freetimers.com>
2022-11-29 17:31:24 +0000

    basic phone validation - convert all phone numbers to digits only, moving any extensions to a separate property

--- classes/SingleItem.php
+++ classes/SingleItem.php
@@ -8,7 +8,26 @@ class SingleItem {
     public function __construct($data) {
         // set an object for each item
         foreach ($data as $key => $item) {
+            // set the item object
             $this->$key = $item;
+
+            // convert any phone numbers into digits
+            if ($key == "phone" && !is_numeric($item)) {
+                // check for any non-numeric characters found in phone numbers
+                $item = str_replace(
+                    ['-', '(', ')', '.'],
+                    '',
+                    $item);
+
+                // split up any extensions
+                $phone_items = explode(" ", $item);
+                unset($phone_items[0]);
+
+                // set the extensions
+                foreach ($phone_items as $extension) {
+                    $this->extensions[] = $extension;
+                }
+            }
         }
     }
 

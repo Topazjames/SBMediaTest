733677f6eaf01e5b93b670e373b401660bbc8437
James Gedny <james.gedny@freetimers.com>
2022-11-29 21:03:02 +0000

    turned off errors

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 // include in the required classes
 include_once (__DIR__ . "/classes/CurlCaller.php");
 include_once (__DIR__ . "/classes/DataHandler.php");

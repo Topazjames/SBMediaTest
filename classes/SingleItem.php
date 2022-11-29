bc67cf96014f0f01b50217820af3159d9a565fc1
James Gedny <james.gedny@freetimers.com>
2022-11-29 20:21:17 +0000

    comments, phone in seperate function, and start of email validation

--- classes/SingleItem.php
+++ classes/SingleItem.php
@@ -3,6 +3,8 @@
 class SingleItem {
 
     /**
+     * Set each item into its own property
+     *
      * @param $data
      */
     public function __construct($data) {
@@ -11,22 +13,38 @@ class SingleItem {
             // set the item object
             $this->$key = $item;
 
-            // convert any phone numbers into digits
-            if ($key == "phone" && !is_numeric($item)) {
-                // check for any non-numeric characters found in phone numbers
-                $item = str_replace(
-                    ['-', '(', ')', '.'],
-                    '',
-                    $item);
+            // validate the phone
+            $this->validatePhone($key, $item);
 
-                // split up any extensions
-                $phone_items = explode(" ", $item);
-                unset($phone_items[0]);
+            // validate the email
+            $this->validateEmail($key, $item);
+        }
+    }
 
-                // set the extensions
-                foreach ($phone_items as $extension) {
-                    $this->extensions[] = $extension;
-                }
+
+    /**
+     * Convert phone numbers into digits, and separate out the extensions into its own property
+     *
+     * @param $key
+     * @param $value
+     * @return void
+     */
+    public function validatePhone ($key, $value) {
+        // convert any phone numbers into digits
+        if ($key == "phone" && !is_numeric($value)) {
+            // check for any non-numeric characters found in phone numbers
+            $value = str_replace(
+                ['-', '(', ')', '.'],
+                '',
+                $value);
+
+            // split up any extensions
+            $phone_items = explode(" ", $value);
+            unset($phone_items[0]);
+
+            // if an extension exists, add the property
+            if (!empty($phone_items)) {
+                $this->extension = $phone_items[1];
             }
         }
     }
